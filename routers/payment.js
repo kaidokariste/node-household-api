6096ffa353f7aaed012e01ac292253cd65484012
Kaido Kariste <kaido.kariste@bigbank.ee>
2018-11-27 14:31:18 +0200

    Grouping added to swagger documentation. HTTP-POST Payments endpoint developed

--- routers/payment.js
+++ routers/payment.js
@@ -4,8 +4,13 @@ controller = require('../controllers'),
 exports.postPayment = function (req, res) {
     const newPaymentObject = new model.Payment(req.body);
     newPaymentObject.save(err => {
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(newPaymentObject);
+        if (!err) {
+            newPaymentObject.save();
+            return res.status(201).send(newPaymentObject);
+        } else {
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.send({'error': 'Internal server error'});
+        }
     });
 }
 
